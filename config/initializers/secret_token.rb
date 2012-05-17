d980ae839e92bf7de9f289d9f28c9e4c975e2647
szatan <szatan@ubuntu.(none)>
2012-05-16 17:21:58 -0700

    Rails app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassManager::Application.config.secret_token = 'c3a79251969c840c3d589bd6f8165595416d691a817cbb226bfd3dd4c5c4bd24867e92fe08b3dd048ddec2824cd3bbc460ac4ef771a9b728607f81d8bf8cc79c'

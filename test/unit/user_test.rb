0857f095bb23e038c072842abcae60f5e067a4be
Szalansky <rafal.szalanski@gmail.com>
2012-05-20 03:32:43 -0700

    Test suite for User model validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  fixtures :users, :passwords
   
   def setup
     @user = User.new
     @user.email = "pat@example.com"
+    @existing_user = users(:existing)
   end
 
   test "should validation pass with password meeting guidelines" do
@@ -35,5 +38,20 @@ class UserTest < ActiveSupport::TestCase
   test "should validation fail with password having too few special characters" do
      @user.password = "aaBB121@PASSWORD"
      assert !@user.valid?
+  end
+
+  test "should password created 14days 1hour ago be expired" do
+    @user.password_updated_at = DateTime.now - 14.days - 1.hours
+    assert @user.password_expired?
+  end
+
+  test "should not password 7days ago be expired" do
+    @user.password_updated_at = DateTime.now - 7.days
+    assert !@user.password_expired?
+  end
+
+  test "should validation fail with password listed as used" do
+    @existing_user.password = "kkBB12!@"
+    assert !@existing_user.valid?
   end
 end
